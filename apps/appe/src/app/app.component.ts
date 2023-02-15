2c7cfb4cf5b6be3df3bd4ef1519ca45f5a870bec
Murat Oguzhan <Murat.Oguzhan@sintec.de>
2023-02-15 15:42:19 +0100

    scss files are added.

--- apps/appe/src/app/app.component.ts
+++ apps/appe/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import sassVariablesExport from '../_export.scss';
 
 @Component({
   selector: 'qwe-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'appe';
+
+  ngOnInit(): void {
+    let qweee = sassVariablesExport;
+  }
 }
